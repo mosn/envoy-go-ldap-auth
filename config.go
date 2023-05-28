97525ea1867a1294d80e07b0f07bfcdd7bc6ff2b
xiaodongzhang <xiaodongzhang@whale.im>
2023-05-29 01:08:36 +0800

    init

---
+++ config.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"github.com/allegro/bigcache/v3"
+	xds "github.com/cncf/xds/go/xds/type/v3"
+	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/api"
+	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/http"
+	"google.golang.org/protobuf/types/known/anypb"
+	"time"
+)
+
+func init() {
+	http.RegisterHttpFilterConfigFactory("envoy-go-ldap-auth", configFactory)
+	http.RegisterHttpFilterConfigParser(&parser{})
+}
+
+type config struct {
+	host      string
+	port      uint64
+	baseDN    string
+	attribute string
+	bindDN    string
+	password  string
+	filter    string
+	cacheTTL  int32
+	timeout   int32
+	cache     *bigcache.BigCache
+}
+
+type parser struct {
+}
+
+func (p *parser) Parse(any *anypb.Any) (interface{}, error) {
+	configStruct := &xds.TypedStruct{}
+	if err := any.UnmarshalTo(configStruct); err != nil {
+		return nil, err
+	}
+
+	v := configStruct.Value
+	conf := &config{}
+	if host, ok := v.AsMap()["host"].(string); ok {
+		conf.host = host
+	}
+	if port, ok := v.AsMap()["port"].(float64); ok {
+		conf.port = uint64(port)
+	}
+	if baseDN, ok := v.AsMap()["base_dn"].(string); ok {
+		conf.baseDN = baseDN
+	}
+	if attribute, ok := v.AsMap()["attribute"].(string); ok {
+		conf.attribute = attribute
+	}
+	if bindDN, ok := v.AsMap()["bind_dn"].(string); ok {
+		conf.bindDN = bindDN
+	}
+	if password, ok := v.AsMap()["bind_password"].(string); ok {
+		conf.password = password
+	}
+	if cFilter, ok := v.AsMap()["filter"].(string); ok {
+		conf.filter = cFilter
+	}
+	if cacheTTL, ok := v.AsMap()["cache_ttl"].(float64); ok {
+		conf.cacheTTL = int32(cacheTTL)
+	}
+	// default is 0, which means no cache
+	var err error
+	if conf.cacheTTL > 0 {
+		conf.cache, err = bigcache.New(
+			context.Background(),
+			bigcache.DefaultConfig(
+				time.Duration(conf.cacheTTL)*time.Second,
+			),
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if timeout, ok := v.AsMap()["timeout"].(float64); ok {
+		conf.timeout = int32(timeout)
+	}
+	if conf.timeout == 0 {
+		conf.timeout = 60
+	}
+	return conf, nil
+}
+
+func (p *parser) Merge(parent interface{}, child interface{}) interface{} {
+	panic("TODO")
+}
+
+func configFactory(c interface{}) api.StreamFilterFactory {
+	conf, ok := c.(*config)
+	if !ok {
+		panic("unexpected config type")
+	}
+	return func(callbacks api.FilterCallbackHandler) api.StreamFilter {
+		return &filter{
+			callbacks: callbacks,
+			config:    conf,
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,24 @@
+module envoy-go-ldap-auth
+
+go 1.19
+
+require (
+	github.com/allegro/bigcache/v3 v3.1.0
+	github.com/cncf/xds/go v0.0.0-20230428030218-4003588d1b74
+	github.com/envoyproxy/envoy v1.26.1
+	github.com/go-ldap/ldap/v3 v3.4.4
+	google.golang.org/protobuf v1.30.0
+)
+
+require (
+	github.com/Azure/go-ntlmssp v0.0.0-20220621081337-cb9428e4ac1e // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.1.0 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.4 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
+	google.golang.org/grpc v1.25.1 // indirect
+)
