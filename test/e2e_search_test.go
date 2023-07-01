0ec9ad65badb33665bd9de8775cf02a07d6e50fa
Makonike <makonike@anyview.fun>
2023-07-01 15:09:00 +0800

    fix: generate config file

--- test/e2e_search_test.go
+++ test/e2e_search_test.go
@@ -1,30 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package test
 
 import (
-	"fmt"
 	"net/http"
-	"os"
-	"os/exec"
 	"testing"
 	"time"
 )
 
-func startEnvoySearch(configPath string) {
-	cmd := exec.Command("envoy", "-c", configPath)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Start()
-	if err != nil {
-		panic(fmt.Sprintf("failed to start envoy: %v", err))
-	}
-	err = cmd.Wait()
-	if err != nil {
-		panic(fmt.Sprintf("failed to wait envoy: %v", err))
-	}
-}
-
 func TestSearch(t *testing.T) {
-	go startEnvoySearch("../example/envoy-search.yaml")
+	startEnvoySearch("localhost", 3893, "dc=glauth,dc=com", "cn", "cn=serviceuser,ou=svcaccts,dc=glauth,dc=com", "mysecret", "(cn=%s)")
 	time.Sleep(5 * time.Second)
 	req, err := http.NewRequest(http.MethodGet, "http://localhost:10000/", nil)
 
--- test/e2e_tls_test.go
+++ test/e2e_tls_test.go
@@ -1,33 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package test
 
 import (
-	"fmt"
 	"net/http"
-	"os"
-	"os/exec"
 	"testing"
 	"time"
 )
 
-func startEnvoyTLS(configPath string) {
-	cmd := exec.Command("envoy", "-c", configPath)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Start()
-	if err != nil {
-		panic(fmt.Sprintf("failed to start envoy: %v", err))
-	}
-	err = cmd.Wait()
-	if err != nil {
-		panic(fmt.Sprintf("failed to wait envoy: %v", err))
-	}
-}
-
 func TestBindTLS(t *testing.T) {
-
-	go startEnvoyTLS("../example/envoy-tls.yaml")
+	startEnvoyTLS("localhost", 3894, "dc=glauth,dc=com", "cn")
 	time.Sleep(5 * time.Second)
-
 	req, err := http.NewRequest(http.MethodGet, "http://localhost:10000/", nil)
 	resp1, err := http.DefaultClient.Do(req)
 	if err != nil {
